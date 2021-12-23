18077219e48bcf495658979c3810cc5154de2aa7
hakjinlee <hakjinlee@si-analytics.ai>
2021-12-23 15:49:23 +0900

    fix glc20 configs

--- configs/_base_/datasets/geococo_glc20_oiltanker.py
+++ configs/_base_/datasets/geococo_glc20_oiltanker.py
@@ -4,7 +4,9 @@ img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375],
 
 train_pipeline = [
     dict(type="LoadImageFromFile"),
-    dict(type="RandomResizedCrop", size=(256, 128)),
+    dict(type="Resize", size=(128, 64)),
+    dict(type="GaussianNoise"),
+    dict(type="Rotate", angle=20.),
     dict(type="RandomFlip", flip_prob=0.5, direction="horizontal"),
     dict(type="RandomFlip", flip_prob=0.5, direction="vertical"),
     dict(type="Normalize", **img_norm_cfg),
@@ -14,14 +16,14 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type="LoadImageFromFile"),
-    dict(type="Resize", size=(256, 128)),
-    # dict(type='CenterCrop', crop_size=128),
+    dict(type="Resize", size=(128, 64)),
+    dict(type="CenterCrop", crop_size=(128, 64)),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="ImageToTensor", keys=["img"]),
     dict(type="Collect", keys=["img"]),
 ]
 data = dict(
-    samples_per_gpu=32,
+    samples_per_gpu=128,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
@@ -43,4 +45,4 @@ data = dict(
         pipeline=test_pipeline,
     ),
 )
-evaluation = dict(interval=1, metric="accuracy", metric_options=dict(topk=(1, 3)))
+evaluation = dict(interval=200, metric="accuracy", metric_options=dict(topk=(1, 3), average_mode="macro"))
