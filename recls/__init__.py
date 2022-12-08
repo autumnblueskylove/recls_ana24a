a162e1585c61362fbb7f7f9cfc7694ca0209bc19
Donggeun Yu <ddonggeunn@gmail.com>
2022-12-08 17:20:22 +0900

    [Fix] import `__version__` in package root (#42)
    * Update recls will have __version__ for MMDeploy
    
    Signed-off-by: Donggeun Yu <ddonggeunn@gmail.com>
    
    * Update recls will have __version__ for MMDeploy
    
    Signed-off-by: Donggeun Yu <ddonggeunn@gmail.com>
    
    Signed-off-by: Donggeun Yu <ddonggeunn@gmail.com>

--- recls/__init__.py
+++ recls/__init__.py
@@ -3,3 +3,6 @@ from .datasets import *  # noqa F403
 from .models import *  # noqa F403
 from .runner import *  # noqa F403
 from .utils import *  # noqa F403
+from .version import __version__
+
+__all__ = ['__version__']
