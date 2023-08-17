02ac1feb9ffba57197a77fd47b31abe52eb587cb
Jamyoung Koo <kjamyoung@gmail.com>
2023-08-17 15:55:31 +0900

    [Feature] Load meta information such as geo-location, date and GSD (#59)
    * [Feature] Load meta information such as geo-location, date and GSD
    * [Update] Remove decoy class

---
+++ configs/_base_/datasets/aircraft/nk-aircraft_224px_w-meta.py
@@ -0,0 +1,134 @@
+dataset_type = 'DataPlatformDataset'
+dp_host = 'http://dev-cluster.sia-service.kr:32160'
+dp_user = 'jmkoo'
+dp_password = 'jmkoo@sia2022'
+
+train_dataset_id = 169
+test_dataset_id = 262
+categories = [
+    dict(id=0, name='AN-148'),
+    dict(id=1, name='AN-2'),
+    dict(id=2, name='AN-24'),
+    dict(id=3, name='Il-18'),
+    dict(id=3, name='IL-18'),
+    dict(id=4, name='Il-28'),
+    dict(id=4, name='IL-28'),
+    dict(id=5, name='Il-62'),
+    dict(id=5, name='IL-62'),
+    dict(id=6, name='Il-76'),
+    dict(id=6, name='IL-76'),
+    dict(id=7, name='Mi-2'),
+    dict(id=7, name='MI-2'),
+    dict(id=8, name='Mi-26'),
+    dict(id=8, name='MI-26'),
+    dict(id=9, name='Mi-4'),
+    dict(id=9, name='MI-4'),
+    dict(id=10, name='Mi-8_17'),
+    dict(id=10, name='MI-8_17'),
+    dict(id=11, name='MiG-15_17'),
+    dict(id=11, name='MIG-15_17'),
+    dict(id=12, name='MiG-19'),
+    dict(id=12, name='MIG-19'),
+    dict(id=13, name='MiG-21'),
+    dict(id=13, name='MIG-21'),
+    dict(id=14, name='MiG-23'),
+    dict(id=14, name='MIG-23'),
+    dict(id=15, name='MiG-29'),
+    dict(id=15, name='MIG-29'),
+    dict(id=16, name='Su-25'),
+    dict(id=16, name='SU-25'),
+    dict(id=17, name='Tu-134'),
+    dict(id=17, name='TU-134'),
+    dict(id=18, name='Tu-154'),
+    dict(id=18, name='TU-154'),
+    dict(id=19, name='Tu-204'),
+    dict(id=19, name='TU-204'),
+    dict(id=20, name='Yak-18'),
+    dict(id=20, name='YAK-18'),
+    dict(id=21, name='Light-Aircraft'),
+    dict(id=21, name='light-aircraft'),
+    dict(id=22, name='Unknown'),
+    dict(id=22, name='unknown'),
+    dict(id=22, name='DECOY')
+]
+
+expand_ratio = 1.2
+data_preprocessor = dict(
+    num_classes=23,
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True,
+)
+
+train_pipeline = [
+    dict(
+        type='JitterRBox',
+        x_range=0.1,
+        y_range=0.1,
+        w_range=(0.9, 1.1),
+        h_range=(0.9, 1.1),
+        rad_range=(-0.174, 0.174)),
+    dict(type='CropInstanceInScene', expand_ratio=expand_ratio),
+    dict(
+        type='RandomStretch',
+        min_percentile_range=(0.0, 1.0),
+        max_percentile_range=(99.0, 100.0)),
+    dict(type='Resize', scale=(224, 224)),
+    dict(
+        type='RandomFlip',
+        prob=0.5,
+        direction=['horizontal', 'vertical', 'diagonal']),
+    dict(
+        type='PackInputs',
+        meta_keys=['img_path', 'ilid', 'longlat', 'date', 'xy_gsd']),
+]
+
+test_pipeline = [
+    dict(type='CropInstanceInScene', expand_ratio=expand_ratio),
+    dict(
+        type='RandomStretch',
+        min_percentile_range=(0.0, 0.0),
+        max_percentile_range=(100.0, 100.0)),
+    dict(type='Resize', scale=(224, 224)),
+    dict(
+        type='PackInputs',
+        meta_keys=['img_path', 'ilid', 'longlat', 'date', 'xy_gsd']),
+]
+
+train_dataloader = dict(
+    batch_size=128,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        pipeline=train_pipeline,
+        host=dp_host,
+        user=dp_user,
+        password=dp_password,
+        dataset_id=train_dataset_id,
+        categories=categories,
+        include_longlat=True,
+        include_date=True,
+        include_gsd=True),
+    sampler=dict(type='DefaultSampler', shuffle=True),
+)
+
+val_dataloader = dict(
+    batch_size=128,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        pipeline=test_pipeline,
+        host=dp_host,
+        user=dp_user,
+        password=dp_password,
+        dataset_id=test_dataset_id,
+        categories=categories,
+        include_longlat=True,
+        include_date=True,
+        include_gsd=True),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+)
+val_evaluator = dict(type='Accuracy', topk=(1, 3))
+
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
--- recls/datasets/dataplatform.py
+++ recls/datasets/dataplatform.py
@@ -2,6 +2,7 @@ from typing import List
 
 from mmpretrain.datasets.base_dataset import BaseDataset
 from mmpretrain.registry import DATASETS
+from osgeo import gdal, osr
 
 
 def get_connection(host: str, user: str, password: str):
@@ -38,8 +39,15 @@ class DataPlatformDataset(BaseDataset):
         password (str): password of user
         rename_class (List[str], optional): Remapping class names to specified
             names
+        include_longlat (bool): whether to include a geolocation(long, lat) of
+            an object.
+        include_date (bool): whether to include date of an image.
+        include_gsd (bool): whether to include GSD(x, y) of an image.
         **kwargs (dict): for BaseDataset
     """
+
+    WGS84_CS = osr.SpatialReference()
+    WGS84_CS.SetWellKnownGeogCS('WGS84')
 
     def __init__(self,
                  dataset_id: int,
@@ -49,6 +57,9 @@ class DataPlatformDataset(BaseDataset):
                  user: str,
                  password: str,
                  rename_class=None,
+                 include_longlat: bool = False,
+                 include_date: bool = False,
+                 include_gsd: bool = False,
                  **kwargs: dict):
         if rename_class is None:
             rename_class = {}
@@ -58,6 +69,10 @@ class DataPlatformDataset(BaseDataset):
         self.dp_client = get_connection(host, user, password)
         self.class_name_to_id = {cat['name']: cat['id'] for cat in categories}
         classes = list(self.class_name_to_id.keys())
+
+        self.include_longlat = include_longlat
+        self.include_date = include_date
+        self.include_gsd = include_gsd
 
         super(DataPlatformDataset, self).__init__(
             '', pipeline=pipeline, classes=classes, **kwargs)
@@ -71,6 +86,13 @@ class DataPlatformDataset(BaseDataset):
         data_list = list()
         for data in dataset:
             filename = data['filepath']
+            if self.include_longlat:
+                raster = gdal.Open(filename)
+                geo_transform = raster.GetGeoTransform(can_return_null=True)
+                coord_transform = osr.CoordinateTransformation(
+                    osr.SpatialReference(raster.GetProjectionRef()),
+                    self.WGS84_CS)
+
             for label in data['labels']:
                 # temporarily avoid label noise
                 if label['width'] > 1 and label['height'] > 1:
@@ -86,5 +108,22 @@ class DataPlatformDataset(BaseDataset):
                         'ilid':
                         label['ilid']
                     }
+                    if self.include_longlat:
+                        if geo_transform:
+                            longlat = gdal.ApplyGeoTransform(
+                                geo_transform, info['rbox'][0],
+                                info['rbox'][1])
+                            longlat = coord_transform.TransformPoint(
+                                longlat[0], longlat[1])
+                            info['longlat'] = longlat[:2]
+                        else:
+                            info['longlat'] = None
+                    if self.include_date:
+                        date = info['img_path'].split('_')[1]
+                        info['date'] = date
+                    if self.include_gsd:
+                        xy_gsd = [data['x_gsd'], data['y_gsd']]
+                        info['xy_gsd'] = xy_gsd
+
                     data_list.append(info)
         return data_list
